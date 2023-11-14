e9b0401b653633ad8fe1268504bcba6b9ce7f742
karolpika <150660953+karolpika@users.noreply.github.com>
2023-11-14 22:36:25 +0100

    Update streamlit_app.py
    meal list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfask Menu')
+streamlit.text('One - first meal')
+streamlit.text('Two - second meal')
+streamlit.text('Three - third meal')
